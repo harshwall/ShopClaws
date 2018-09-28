adff2904fd7ebe2888b30e9aeea9b1fae9887b78
jankit311 <jankit311@gmail.com>
2018-09-28 12:11:44 +0530

    Some crash modified

---
+++ app/src/main/java/com/kunal/shopclaws/Utility/NotificationData.java
@@ -0,0 +1,34 @@
+package com.kunal.shopclaws.Utility;
+
+public class NotificationData {
+    String message;
+    String timestamp;
+    String sender;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+
+
+}
